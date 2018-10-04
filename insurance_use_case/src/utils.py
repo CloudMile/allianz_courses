a940e2244b0e66f7d99bb7dab173ba272932974e
gary.chen <gary.chen@mile.cloud>
2018-10-04 18:03:26 +0800

    #

--- insurance_use_case/src/utils.py
+++ insurance_use_case/src/utils.py
@@ -1,8 +1,5 @@
 from matplotlib import pyplot as plt
-
-from keras.layers import Dense
-from tensorflow.python.keras.utils.data_utils import get_file
-from seaborn import kdeplot
+import numpy as np
 
 def plot_result(hist):
     plt.figure(figsize=(16, 5))
@@ -19,3 +16,33 @@ def plot_result(hist):
 
     plt.legend(loc='best')
     plt.show()
+
+
+def woe_encode(x, label, data):
+    """Calculate the Weight of Evidence of given categorical feature and label
+
+    :param x: Given feature name
+    :param label: Label name
+    :param data:
+    :return: Woe encoded dictionary
+    """
+    total_vc = data[label].value_counts().sort_index()
+
+    def woe(pipe, total_vc):
+        # Count by label in this group
+        group_vc = pipe[label].value_counts().sort_index()
+
+        # Some class in the feature is missing, fill zero to missing class
+        if len(group_vc) < len(total_vc):
+            for key in total_vc.index:
+                if key not in group_vc:
+                    group_vc[key] = 0.
+            group_vc = group_vc.sort_index()
+
+        # WOE formula
+        r = ((group_vc + 0.5) / total_vc).values
+
+        # Odd ratio => 1 to 0, you can define meaning of each class
+        return np.log(r[1] / r[0])
+
+    return data.groupby(x).apply(lambda pipe: woe(pipe, total_vc))
